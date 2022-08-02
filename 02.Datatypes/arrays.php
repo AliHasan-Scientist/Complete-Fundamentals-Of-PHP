391aa6bb4db6e39b24a9bc0fe747f2b201cef8fc
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-08-03 02:50:27 +0500

    Datatypes -> start

---
+++ 02.Datatypes/arrays.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Arrays </title>
+</head>
+<body>
+<?php
+	$programming_lang=array("PHP","Java","Python","C#");
+print($programming_lang[1]);
+// type
+print("<br>");
+print(var_dump( $programming_lang))
+
+?>
+</body>
+</html>
---
+++ 02.Datatypes/string.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	$str1="try try";
+	$str2="again!";
+// we can concat two strings using . operator
+	$concat=$str1 ." " .$str2;
+	print $concat;
+	// var_dump functioin return the data type of variable...
+	print var_dump($concat);
+	
+	?>
+</body>
+</html>
