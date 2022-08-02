9756937b1bb5ae7c6e2bdda2cbc9016284729d5a
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-08-02 21:29:46 +0000

    Variables -> finished

--- 01.Variables/variables.php
+++ 01.Variables/variables.php
@@ -3,13 +3,34 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Variables in PHP</title>
 </head>
 <body>
 	<?php
-	$var1 =2;
-	echo $var1;
+	// variable declaration in PHP
+	$number =2;
+	// floatin point number
+	$point =1.2222;
+
+	echo $number;
 	
+	// addition of numbers
+	$sum_of_numbers=$number+ $point;
+    echo $sum_of_numbers;
+	//String 
+
+	$name=" <br >Roy Ali Hasan";
+$last_name="Mubarak Ali";
+
+	echo $name ." <br> " .$last_name;
+// We can Also use print function in php
+print " <br>|--This is print function--| ";
+
+	/**
+	 * Multi-line comments in PHP
+	 * 
+	 * 
+	 */
 	
 	?>
 </body>
