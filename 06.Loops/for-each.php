3cc7fba3c197d4fee099835463e7801b94611878
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-08-03 08:31:25 +0500

    function + arrays -> learn + updating

---
+++ 06.Loops/for-each.php
@@ -0,0 +1,7 @@
+<?php
+$programing_languages=array("PHP","Python","Java","C++");
+foreach ($programing_languages as $p_languages) {
+	echo $p_languages . " , ";
+}
+echo " <br> array lenght: " . count($programing_languages);
+?>
---
+++ 07.functions/method.php
@@ -0,0 +1,16 @@
+<?php
+// syntax 
+
+
+// strcit mode enable
+declare(strict_types=1);
+function sayHello( string $name)
+{
+	return "Hello " . " -- ". $name;
+}
+
+
+
+echo sayHello("Roy Ali Hasan");
+
+?>
