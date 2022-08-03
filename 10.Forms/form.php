a0116d9d20551bc30ef30d9d66094908ef5d3c23
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-08-03 12:51:16 +0500

    Form Submission + MySql => start

---
+++ 10.Forms/form.php
@@ -0,0 +1,36 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD']) {
+	$f_name=$_POST["First_Name"];
+	$last_name=$_POST["Last_Name"];
+	echo $f_name;
+    echo "<br>";
+	echo $last_name;
+	// Connecting to database
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$datatbase="mysite";
+	 $conn=	mysqli_connect($servername,$username,$password,$datatbase);
+	 if (!$conn) {
+		die("connection failed!". mysqli_connect_error());
+	 }
+else 	 echo " <br> successfull";
+
+
+	} 
+	// create a new databae
+	// $sql="CREATE DATABASE students_record1";
+    // $check=	mysqli_query($conn,$sql);
+	// // data base creation Successfull!
+	// $result = ($check) ? "<br>Db is create succesfully" : "<br>Db cannot be created Sorry!" ;
+	// echo $result;
+    // create a new table
+	$sql = "CREATE TABLE `student_results` (`s_id` INT(40) NOT NULL AUTO_INCREMENT , `first_name` VARCHAR(40) NOT NULL , `last_name` VARCHAR(40) NOT NULL , `email` VARCHAR(35) NOT NULL , PRIMARY KEY (`s_id`)) ENGINE = InnoDB;";
+$result =mysqli_query($conn ,$sql);
+if ($result) {
+	echo "created";
+} else {
+	echo "not create";
+}
+
+?>
