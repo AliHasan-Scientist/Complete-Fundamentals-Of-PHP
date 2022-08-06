64d09476c211103acd6560442d30a44855513759
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-08-06 08:00:28 +0500

    db connection -> [ changed ]

--- 15.REST-APIs/_db-connection.php
+++ 15.REST-APIs/_db-connection.php
@@ -2,8 +2,8 @@
 $servername="localhost";
 $username="root";
 $password="";
-$datatbase="notes-taker";
- $conn=	mysqli_connect($servername,$username,$password,$datatbase);
+$database="notes-taker";
+ $conn=	mysqli_connect($servername,$username,$password,$database);
  if (!$conn) {
 	die("connection failed!". mysqli_connect_error()); }
 else 	 echo "";
